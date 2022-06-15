d52b002695168ebcdb9ce3dee48844dab89a6546
Ayush Singh <ayush.singh21082@gmail.com>
2022-06-15 14:49:53 +0530

    Initial Commit

---
+++ MusicKit Introduction/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicKit Introduction
+//
+//  Created by Ayush Singh on 15/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicKit Introduction/MusicKit_IntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicKit_IntroductionApp.swift
+//  MusicKit Introduction
+//
+//  Created by Ayush Singh on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicKit_IntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
